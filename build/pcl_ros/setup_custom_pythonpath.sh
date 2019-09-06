730e2446f7646e847821dfb345f72af7c00f55b7
Quack <twbabyduck@gmail.com>
2019-09-07 00:19:51 +0800

    .gitignore fix

--- build/pcl_ros/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/nvidia/Library/ros_pcl/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
